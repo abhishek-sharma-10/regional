dc9d7af02bdff5202e7a5791218a13c040218c73
abhishek-sharma-10 <abhishek.sharma@ibirdsservices.com>
2025-04-28 23:59:30 +0530

    Header Changes

--- app/Views/student/template/header.php
+++ app/Views/student/template/header.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>REGIONAL INSTITUTE OF EDUCATION, AJMER</title>
+  <title>RIE, AJMER</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
