5d4d29890fb5c7a847bd6a633c9e275651dc41b1
abhishek-sharma-10 <abhishek.sharma@ibirdsservices.com>
2025-04-28 23:48:36 +0530

    Update Student Login

--- app/Views/student/login/login.php
+++ app/Views/student/login/login.php
@@ -1,50 +1,71 @@
+<html>
 
-    <style>
-        body {
-            background-color: #fff;
-        }
-    
-        .main-div{
-            width: 50%;
-            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-            margin-bottom: 2rem;
-            margin-top: 2rem;
-            padding: 30px 15px 15px;
-        }
-        .login-container {
-          background-color: #2a437a;
-          padding: 30px;
-          border-radius: 10px;
-          max-width: 400px;
-          margin: 0px auto;
-          color: white;
-        }
-        .form-control::placeholder {
-          color: #6c757d;
-        }
-        .form-login-btn {
-          background-color: orange;
-          border: none;
-          width: 100%;
-        }
-        .form-login-btn:hover {
-         background-color: darkorange;
-        }
-        .login-footer {
-          font-size:14px;
-          font-weight: 600;
-        }
-        .login-footer p {
-            margin-bottom: 5px;
-        }
-        .login-footer a {
-          color: red;
-          text-decoration: none;
-          font-weight: 900;
-        }
-    </style>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>RIE | <?php echo $pageTitle; ?></title>
+  <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/starinfotechcollegelogo.png" />
 
-    <div class="container main-div">
+  <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/font-awesome/css/font-awesome.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/css/animate.css" rel="stylesheet">
+  <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
+  <script src="<?php echo base_url(); ?>assets/js/jquery-2.1.1.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/jquery-ui.min.css">
+
+  <script src="<?php echo base_url(); ?>assets/js/plugins/jquery-ui/jquery-ui.min.js"></script>
+
+</head>
+
+<style>
+  body {
+    background-color: #fff;
+  }
+
+  .main-div {
+    width: 50%;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    margin-bottom: 2rem;
+    margin-top: 2rem;
+  }
+
+  .login-container {
+    background-color: #2a437a;
+    padding: 30px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 60px auto;
+    color: white;
+  }
+
+  .form-control::placeholder {
+    color: #6c757d;
+  }
+
+  .login-btn {
+    background-color: orange;
+    border: none;
+    width: 100%;
+  }
+
+  .login-btn:hover {
+    background-color: darkorange;
+  }
+
+  .login-footer {
+    font-size: 16px;
+    font-weight: 900;
+  }
+
+  .login-footer a {
+    color: red;
+    text-decoration: none;
+  }
+</style>
+
+<body>
+  <div class="container main-div" style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;">
     <div class="login-container text-center">
       <form id="loginForm" method="post" action="<?php echo base_url(); ?>login">
         <div class="mb-3 text-start">
@@ -56,29 +77,31 @@
           <input type="password" class="form-control" id="password" placeholder="Password" name="password">
         </div>
         <?php
-            if(isset($invalid) && $invalid == true){
-        ?>						
-            <font color="#FF0000" face="Verdana">* Either username or password mismatch or your system is not valid to use this software.</font>
-        <?php
-            }
+        if (isset($invalid) && $invalid == true) {
         ?>
-        <button type="submit" class="btn form-login-btn text-white mt-2">Login</button>
+          <font color="#FF0000" face="Verdana">* Either username or password mismatch or your system is not valid to use this software.</font>
+        <?php
+        }
+        ?>
+        <button type="submit" class="btn login-btn text-white mt-2">Login</button>
       </form>
     </div>
 
     <div class="text-center mt-3 login-footer">
-      <p>Forget Password ? <a href="#">Click here</a></p>
+      <p>Forget Password ? <a href="<?php echo base_url('forget-password'); ?>">Click here</a></p>
       <p>If not Registered yet <a href="<?php echo base_url('registrations'); ?>">Click here</a></p>
     </div>
   </div>
+</body>
+<script src="<?php echo base_url(); ?>assets/js/jquery.validate.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/dataTables/jquery.dataTables.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/dataTables/dataTables.bootstrap.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/dataTables/dataTables.responsive.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/plugins/dataTables/dataTables.tableTools.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $("#loginForm").validate();
+  });
+</script>
 
-    <!--<script src="<?php echo base_url();?>assets/js/jquery.validate.js"></script>-->
-    <!--<script src="<?php echo base_url();?>assets/js/plugins/dataTables/jquery.dataTables.js"></script>-->
-    <!--<script src="<?php echo base_url();?>assets/js/plugins/dataTables/dataTables.bootstrap.js"></script>-->
-    <!--<script src="<?php echo base_url();?>assets/js/plugins/dataTables/dataTables.responsive.js"></script>-->
-    <!--<script src="<?php echo base_url();?>assets/js/plugins/dataTables/dataTables.tableTools.min.js"></script>-->
-    <script>
-        // $(document).ready(function(){
-        //     $("#loginForm").validate();
-        // });
-    </script>
+</html>
