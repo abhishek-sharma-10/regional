0e99b32497a6ec5ac5c06338cbe2e747d5946ffc
Abhishek Sharma <abhishek.sharma@ibirdsservices.com>
2025-05-12 13:28:41 +0530

    Student

--- app/Views/student/registrations/registrations.php
+++ app/Views/student/registrations/registrations.php
@@ -443,15 +443,12 @@ if ($register_container) {
     });
   });
 
-  const togglePassword = document
-			.querySelector('#togglePassword');
+  const togglePassword = document.querySelector('#togglePassword');
 		const password = document.querySelector('#password');
 		togglePassword.addEventListener('click', () => {
 			// Toggle the type attribute using
 			// getAttribure() method
-			const type = password
-				.getAttribute('type') === 'password' ?
-				'text' : 'password';
+			const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
 			password.setAttribute('type', type);
 			// Toggle the eye and bi-eye icon
 			this.classList.toggle('bi-eye');
