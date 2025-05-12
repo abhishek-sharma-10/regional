1eeff25c028c1b67a70b5c24e7dd0fa550019334
Abhishek Sharma <abhishek.sharma@ibirdsservices.com>
2025-05-12 15:13:34 +0530

    Diable Console

--- app/Views/student/template/footer.php
+++ app/Views/student/template/footer.php
@@ -37,6 +37,18 @@
                     policyModal.hide();
                 }
             });
+
+            // Disable right click
+            document.addEventListener('contextmenu', function(e){
+                e.preventDefault();
+            });
+
+            // Disable F12 key
+            document.addEventListener('keydown', function(e){
+                if(e.key === 'F12' || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J' || e.key === 'C')) || (e.ctrlKey && e.key === 'U')) {
+                    e.preventDefault();
+                }
+            });
         </script>
     </body>
 </html>
