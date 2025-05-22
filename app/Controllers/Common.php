868f085cdb1f84b0e44c009f5e763d7edcae1972
Abhishek Sharma <abhishek.sharma@ibirdsservices.com>
2025-05-22 19:53:57 +0530

    Fixes

--- app/Controllers/Common.php
+++ app/Controllers/Common.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\RegistrationModel;
+
 class Common extends BaseController
 {
     public function __construct()
@@ -12,8 +14,11 @@ class Common extends BaseController
     public function getInstruction(){
         $data['pageTitle'] = "Registrations";
         $data['active'] = '';
-        $id = isset($_SESSION['student']) ? $_SESSION['student'][0]->id : '';
-        $data['details'] = (object)['id' => $id];
+        $registrationModel = new RegistrationModel();
+        if(isset($_SESSION['student'])){
+            $details = $registrationModel->getRegistrationDetail($_SESSION['student'][0]->id);
+            $data['details'] = $details;
+        }
         return view('student/template/header', $data) . view('student/registrations/instruction', $data) . view('student/template/footer');
     }
 
@@ -21,8 +26,11 @@ class Common extends BaseController
         $data['pageTitle'] = "Contact Us";
         $data['active'] = '';
 
-        $id = isset($_SESSION['student']) ? $_SESSION['student'][0]->id : '';
-        $data['details'] = (object)['id' => $id];
+        $registrationModel = new RegistrationModel();
+        if(isset($_SESSION['student'])){
+            $details = $registrationModel->getRegistrationDetail($_SESSION['student'][0]->id);
+            $data['details'] = $details;
+        }
 
         return view('student/template/header', $data) . view('student/contact_us', $data) . view('student/template/footer');
     }
