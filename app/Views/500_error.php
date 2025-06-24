60929c573bacb66bba64c3e3ce7a65f3b7b82747
Abhishek Sharma <abhishek.sharma@ibirdsservices.com>
2025-06-24 17:04:14 +0530

    Site Down

--- app/Views/500_error.php
+++ app/Views/500_error.php
@@ -16,15 +16,15 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>assets/css/animate.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>public/assets/css/animate.css" rel="stylesheet">
 
     <!-- Template Main CSS File -->
-    <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/css/student_style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/assets/css/style.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>public/assets/css/student_style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script src="<?php echo base_url(); ?>assets/js/jquery-2.1.1.js"></script>
+    <script src="<?php echo base_url(); ?>public/assets/js/jquery-2.1.1.js"></script>
 
     <style>
         body {
